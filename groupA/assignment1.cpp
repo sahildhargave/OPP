e23aadb3447a49b385c4cbee316198ce68b4ed2a
sahil dhargave <98769937+SD3691@users.noreply.github.com>
2022-12-17 15:13:01 +0530

    Update assignment1.cpp

--- groupA/assignment1.cpp
+++ groupA/assignment1.cpp
@@ -1,3 +1,20 @@
+/*
+Title :- Define a class to represent a bank account which includes the following
+members as:
+Data members:
+a. Name of the depositor
+b. Account Number
+c. Withdrawal amount
+d. Balance amount in the account Member Functions:
+e. To assign initial values
+f. To deposit an amount
+g. To withdraw an amount after checking the balance
+h. To display name and balance.
+
+Author  :- Sahil Dhargave
+
+*/
+
 #include <iostream>
 
 using namespace std;
