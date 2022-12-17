c8475e0ef1ad6cec0538ac624d6d3c15c9ed181f
sahil dhargave <98769937+SD3691@users.noreply.github.com>
2022-12-17 15:06:06 +0530

    Update assignment5.cpp

--- groupA/assignment5.cpp
+++ groupA/assignment5.cpp
@@ -1,3 +1,15 @@
+/*
+Title   :-  Create a base class called 'SHAPE' having-two data members of type double -member function get-data( )to
+initialize base class data members - pure virtual member function display-area( )to compute and display the
+area of the geometrical object.
+Derive two specific classes 'TRIANGLE' and 'RECTANGLE' from the base class Using these three
+classes, design a program that will accept the dimension of a triangle / rectangle interactively and display
+the area. Implement using C++.
+
+Author  :-  Sahil Dhargave
+
+*/
+
 #include <iostream>
 using namespace std;
 
