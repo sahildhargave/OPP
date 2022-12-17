852b75658549417057340c25a38d961f0b312133
sahil dhargave <98769937+SD3691@users.noreply.github.com>
2022-12-17 15:10:06 +0530

    Update assignment3.cpp

--- groupA/assignment3.cpp
+++ groupA/assignment3.cpp
@@ -1,3 +1,22 @@
+/*
+Title  :-  Consider we want to store the information of different vehicles.
+Create a class named Vehicle with two data member named mileage and price. Create its two
+subclasses:
+*Car with data members to store ownership cost, warranty (by years), seating capacity and fuel type
+(diesel or petrol).
+*Bike with data members to store the number of cylinders, number of gears, cooling type(air, liquid or
+oil), wheel type(alloys or spokes) and fuel tank size(in inches).
+Make another two subclasses Audi and Ford of Car, each having a data member to store the model type.
+Next, make two subclasses Bajaj and TVS, each having a data member to store the make-type. Now,
+store and print the information of an Audi and a Ford car (i.e. model type, ownership cost, warranty,
+seating capacity, fuel type, mileage and price.).
+
+Author  :-  Sahil Dhargave
+
+
+
+*/
+
 #include <iostream>
 using namespace std;
 
