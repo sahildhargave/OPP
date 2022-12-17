312d03d8071664370ef4cb6d231bfc4b25cb23c2
sahil dhargave <98769937+SD3691@users.noreply.github.com>
2022-12-17 15:04:26 +0530

    Update assignment7.cpp

--- groupA/assignment7.cpp
+++ groupA/assignment7.cpp
@@ -1,3 +1,13 @@
+/*
+Title  :-  Write a program to maintain an employee database in binary file with employee information such as empId,
+name, age, department, post and salary. Write function for adding new record, displaying all records,
+searching for a particular employee, updating employee salary and post.
+
+Author  :- Sahil Dhargave
+
+*/
+
+
 #include <iostream>
 using namespace std;
 
