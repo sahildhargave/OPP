89da65712d62f0cacf30724b18b9af64eb400ede
sahil dhargave <98769937+SD3691@users.noreply.github.com>
2023-01-08 19:41:30 +0530

    Update assignment10.cpp

--- groupB/assignment10.cpp
+++ groupB/assignment10.cpp
@@ -8,6 +8,152 @@ d. Set of customers who like only burger not
 pizza.
 e. Number of customers who like neither pizza nor burger.
 */
+
+Methode -1
+	
+	
+#include<bits/stdc++.h>
+using namespace std;
+
+class customer
+{
+    int id[10];
+    int n;
+    vector <string>  name;
+    public: 
+    void accept();
+    void display();
+    friend void union_(customer &,customer &);
+    friend void intersection_(customer &,customer &);
+    friend void only_pizza(customer &,customer &);
+    friend void only_burger(customer &,customer &);  
+};
+ 
+void union_(customer & pizza,customer & burger)
+{
+
+    int marked[100]={0};
+    for(int i=0;i<pizza.n;i++)
+    {
+        cout<<pizza.id[i]<<" "<<pizza.name[i]<<endl;
+        marked[pizza.id[i]]=1;
+    }
+    for(int i=0;i<pizza.n;i++)
+    {
+    for(int j=0;j<burger.n;j++)
+    {
+        if(pizza.id[i]!=burger.id[j]&&marked[burger.id[j]]==0)
+        {
+            marked[burger.id[j]]=1;
+            cout<<burger.id[j]<<" "<<burger.name[j]<<endl;
+        }
+    }
+    }
+}
+void intersection_(customer & pizza,customer & burger)
+{
+    int marked[100]={0};
+    for(int i=0;i<pizza.n;i++)
+    {
+        for(int j=0;j<burger.n;j++)
+        {
+            if(pizza.id[i]==burger.id[j]&& marked[pizza.id[i]]==0)
+            {
+                marked[pizza.id[i]]=1;
+                cout<<pizza.id[i]<<" "<<pizza.name[j]<<endl;
+            }
+        }
+    }
+}
+void only_pizza(customer &pizza,customer &burger)
+{
+    int marked[100]={0};
+   for(int i=0;i<pizza.n;i++)
+    {
+        for(int j=0;j<burger.n;j++)
+        {
+            if(pizza.id[i]==burger.id[j]&& marked[pizza.id[i]]==0)
+            {
+                marked[pizza.id[i]]=1;
+            }
+        }
+    }
+    for(int i=0;i<pizza.n;i++)
+    {
+        if(marked[pizza.id[i]]!=1)
+        {
+            cout<<pizza.id[i]<<" "<<pizza.name[i]<<endl;
+        }
+    }
+}
+void only_burger(customer &pizza,customer &burger)
+{
+    int marked[100]={0};
+   for(int i=0;i<pizza.n;i++)
+    {
+        for(int j=0;j<burger.n;j++)
+        {
+            if(pizza.id[i]==burger.id[j]&& marked[pizza.id[i]]==0)
+            {
+                marked[burger.id[i]]=1;
+            }
+        }
+    }
+    for(int i=0;i<burger.n;i++)
+    {
+        if(marked[burger.id[i]]!=1)
+        {
+            cout<<burger.id[i]<<" "<<burger.name[i]<<endl;
+        }
+    }
+}
+void  customer:: accept()
+{
+    cout<<"Enter no. of customers "<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter ID: ";
+        cin>>id[i];
+        cout<<"Enter Name : ";
+        string s;
+        cin>>s;
+        name.push_back(s);
+    }
+}
+void customer :: display()
+{
+     for(int i=0;i<n;i++)
+    {
+    cout<<"ID = "<<id[i]<<"  Name = "<<name[i]<<endl;
+    }
+}
+
+int main()
+{
+    customer pizza;
+    customer burger;
+    cout<<"For Pizza"<<endl;
+    pizza.accept();
+    pizza.display();
+    burger.accept();
+    cout<<"For Burger"<<endl;
+    burger.display();
+    cout<<endl;
+    cout<<"Customers who like either pizza or burger "<<endl;
+    union_(pizza,burger);
+    cout<<"Customers who like both pizza and burger"<<endl;
+    intersection_(pizza,burger);
+    cout<<"Customer who like only pizza"<<endl;
+    only_pizza(pizza,burger);
+    cout<<"Customer who like only burger"<<endl;
+    only_burger(pizza,burger);
+    return 0;
+}
+
+
+
+
 #include <iostream>
 
 using namespace std;
